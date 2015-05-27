697f556dc0fa38b1da75ba03f79b378b195e0d4a
Helge Langehaug <hellange@cisco.com>
2015-05-27 18:42:10 +0200

    Added http request to weather server

--- src/com/example/knottetaxi_android/MainActivity.java
+++ src/com/example/knottetaxi_android/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.knottetaxi_android;
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,6 +37,26 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View arg0) {        
                 // create class object
+            	
+            	Log.i("MainActivity", "onclick ");
+                HttpClient httpclient = new DefaultHttpClient();
+
+                HttpGet request = new HttpGet();
+                try {
+                URI website = new URI("http://weather2-helge.rhcloud.com/weather/history/0?format=json");
+                request.setURI(website);
+                HttpResponse response = httpclient.execute(request);
+                HttpEntity entity = response.getEntity();
+                String responseString = EntityUtils.toString(entity, "UTF-8");
+                
+            	Log.e("MainActivity", "Response : "+responseString);
+
+                } catch (Exception ex) {
+                	Log.e("MainActivity", "Exception : "+ex.getMessage());
+
+                }	
+   
+                
                 gps = new GPSTracker(MainActivity.this);
  
                 // check if GPS enabled     
